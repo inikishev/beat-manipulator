81a3cc7a7a88a834f7090bf10857712286e90966
dpe <76593873+dpe0@users.noreply.github.com>
2022-11-22 22:23:45 +0300

    da baby

--- example.py
+++
@@ -1,8 +0,0 @@
-import BeatManipulator as bm
-
-# dnb 1=khshkhsh, or khsh
-# rest - khsh
-
-dotted05='0:3, 4:4.5,7, 4:4.5,11, 4:4.5,7, 4:4.5,11, 8:8.5,7, 8:8.5,11, 8:8.5,7, 8:8.5,11, 16' #0.125 - 0.5
-
-bm.wrapper_beatswap(filename=None, pattern=dotted05, scale=0.125, shift=0, start=0, end=None)
